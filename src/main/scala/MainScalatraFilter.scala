a37bcc92a0e841f016ebeee1f39bfceea84376c6
Oscar Renalias <oscar@renalias.net>
2011-12-19 00:54:45 +0200

    Some improvements, but it still doesn't work

--- src/main/scala/MainScalatraFilter.scala
+++ src/main/scala/MainScalatraFilter.scala
@@ -4,16 +4,13 @@ import org.scalatra._
 
 import com.google.appengine.api.conversion.Asset;
 import com.google.appengine.api.conversion.Conversion;
-import com.google.appengine.api.conversion.ConversionResult;
-import com.google.appengine.api.conversion.ConversionService;
 import com.google.appengine.api.conversion.ConversionServiceFactory;
 import com.google.appengine.api.conversion.Document;
-import org.scalatra.fileupload.FileUploadSupport
 import org.apache.commons.io.IOUtils
 import scala.collection.JavaConversions._
 import org.apache.commons.fileupload.servlet.ServletFileUpload
-import org.apache.commons.fileupload.FileItemStream
 import javax.servlet.http.HttpServletRequest
+import collection.mutable.HashMap
 
 class MainScalatraFilter extends ScalatraFilter {
   
@@ -42,23 +39,33 @@ class MainScalatraFilter extends ScalatraFilter {
     }
 
     val fileType = Map("png" -> "image/png", "jpg" -> "image/jpg", "jpeg" -> "image/jpeg") // TODO: what happens if the extension is none of these?
-    
-    def handleUpload(req: HttpServletRequest): Option[(String, Array[Byte])] = {
+
+    /**
+     * Scalatra's file upload capabilities depend on Commons' FileUpload, which is not fully supported
+     * in GAE, so we need to find our way around that...
+     */
+    case class FileUpload(name:String, size:Int, data:Array[Byte])
+
+    def handleUpload(req: HttpServletRequest): scala.collection.mutable.Map[String, FileUpload] = {
       val upload = new ServletFileUpload();
       val iterator = upload.getItemIterator(req);
+      val results = new HashMap[String, FileUpload]()
 
-      // TODO: quick and dirty for now, but what if there's nothing to iterate?
-      val item = iterator.next();
+      while(iterator.hasNext) { // TODO: we can probably Scala-ify this bit...
+        val item = iterator.next();
+    
+        if (!item.isFormField()) {
+          val contents = IOUtils.toByteArray(item.openStream())
+          results += (item.getFieldName -> FileUpload(item.getName, contents.size, contents))
+        }
+      }
 
-      if (item.isFormField())
-        None
-      else
-        Some((item.getName, IOUtils.toByteArray(item.openStream())))
+      results
     }
 
-    handleUpload(this.request).map({case (fileName, fileContent)=>
+    handleUpload(this.request).get("fileToScan").map({fileData =>
       // note: we use Commons' IOUtils to make our life easier to convert the input stream to byte[]
-      val asset = new Asset(fileType(fileExt(fileName)), fileContent, fileName)
+      val asset = new Asset(fileType(fileExt(fileData.name)), fileData.data, fileData.name)
       val document = new Document(asset)
       val conversion = new Conversion(document, "text/html")
       val result = ConversionServiceFactory.getConversionService().convert(conversion)
